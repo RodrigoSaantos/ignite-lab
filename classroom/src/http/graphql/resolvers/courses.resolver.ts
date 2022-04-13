be6e0362001b44cc8e411029f989583dda1970ea
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-13 00:20:27 -0300

    List courses graphql

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,5 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { CoursesService } from '../../../services/courses.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
-export class CoursesResolver {}
+export class CoursesResolver {
+  constructor(private coursesService: CoursesService) {}
+
+  @Query(() => [Course])
+  @UseGuards(AuthorizationGuard)
+  courses() {
+    return this.coursesService.listAllCourses();
+  }
+}
--- classroom/src/services/courses.service.ts
+++ classroom/src/services/courses.service.ts
@@ -4,4 +4,7 @@ import { PrismaService } from '../database/prisma/prisma.service';
 @Injectable()
 export class CoursesService {
   constructor(private prisma: PrismaService) {}
+  listAllCourses() {
+    return this.prisma.course.findMany();
+  }
 }
