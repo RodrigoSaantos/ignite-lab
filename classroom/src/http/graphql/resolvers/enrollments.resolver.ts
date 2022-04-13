1597d5b9a70fcedc77a0dcfc7f4405fbdd336f3a
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-13 00:25:24 -0300

    List enrollments graphql

--- classroom/src/http/graphql/resolvers/enrollments.resolver.ts
+++ classroom/src/http/graphql/resolvers/enrollments.resolver.ts
@@ -1,5 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { EnrollmentsService } from '../../../services/enrollments.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
 import { Enrollment } from '../models/enrollment';
 
 @Resolver(() => Enrollment)
-export class EnrollmentsResolver {}
+export class EnrollmentsResolver {
+  constructor(private enrollmentsService: EnrollmentsService) {}
+
+  @Query(() => [Enrollment])
+  @UseGuards(AuthorizationGuard)
+  enrollments() {
+    return this.enrollmentsService.listAllEnrollments();
+  }
+}
--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -4,4 +4,15 @@ import { PrismaService } from '../database/prisma/prisma.service';
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
+
+  listAllEnrollments() {
+    return this.prisma.enrollment.findMany({
+      where: {
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
 }
