5fe9cc8ac5c65faaca32e3b88824c3e72cc51198
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-17 23:07:59 -0300

    Register course

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,7 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CoursesService } from '../../../services/courses.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { CreateCourseInput } from '../inputs/create-course-input';
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
@@ -12,5 +13,11 @@ export class CoursesResolver {
   @UseGuards(AuthorizationGuard)
   courses() {
     return this.coursesService.listAllCourses();
+  }
+
+  @Mutation(() => Course)
+  @UseGuards(AuthorizationGuard)
+  createCourse(@Args('data') data: CreateCourseInput) {
+    return this.coursesService.createCourse(data);
   }
 }
--- classroom/src/services/courses.service.ts
+++ classroom/src/services/courses.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PrismaService } from '../database/prisma/prisma.service';
+
+interface CreateCourseParams {
+  title: string;
+}
 
 @Injectable()
 export class CoursesService {
@@ -11,6 +16,29 @@ export class CoursesService {
   getCourseById(id: string) {
     return this.prisma.course.findUnique({
       where: { id },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseParams) {
+    const slug = slugify(title, {
+      lower: true,
+    });
+
+    const courseAlreadyExists = await this.prisma.course.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (courseAlreadyExists) {
+      throw new Error('Course already exists.');
+    }
+
+    return this.prisma.course.create({
+      data: {
+        title,
+        slug,
+      },
     });
   }
 }
