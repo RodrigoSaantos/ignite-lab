5e0f132729b6539c9d67d12b93c6ec4b8384bbe8
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-18 23:19:36 -0300

    Create a event to kafka

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -10,11 +10,13 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 import { PurchasesService } from '../services/purchases.service';
 import { CustomersService } from '../services/customers.service';
 import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { MessagingModule } from '../messaging/messaging.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    MessagingModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
