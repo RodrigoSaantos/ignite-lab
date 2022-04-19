13c8adba15528e598026db351507a310add5213a
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-19 00:05:29 -0300

    Register with purchases and classroom

--- classroom/src/services/students.service.ts
+++ classroom/src/services/students.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
-
+interface CreateStudentParams {
+  authUserId: string;
+}
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
@@ -19,6 +21,14 @@ export class StudentsService {
   getStudentById(id: string) {
     return this.prisma.student.findUnique({
       where: { id },
+    });
+  }
+
+  createStudent({ authUserId }: CreateStudentParams) {
+    return this.prisma.student.create({
+      data: {
+        authUserId,
+      },
     });
   }
 }
