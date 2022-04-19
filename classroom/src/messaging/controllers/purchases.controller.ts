0021c359ebbee9007208ba3ca5606e113aed6092
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-18 23:36:39 -0300

    Receive event from kafka

---
+++ classroom/src/messaging/controllers/purchases.controller.ts
@@ -0,0 +1,25 @@
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+export interface Customer {
+  authUserId: string;
+}
+
+export interface Product {
+  id: string;
+  title: string;
+  slug: string;
+}
+
+export interface PurchaseCreatePayload {
+  customer: Customer;
+  product: Product;
+}
+
+@Controller()
+export class PurchaseController {
+  @EventPattern('purchases.new-purchase')
+  async purchaseCreated(@Payload('value') payload: PurchaseCreatePayload) {
+    console.log(payload);
+  }
+}
