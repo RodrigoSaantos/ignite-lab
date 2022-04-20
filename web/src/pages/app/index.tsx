88d2d3a938c94ed1dc77f7fdb028186d953125e3
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-04-19 23:15:35 -0300

    Config apolloClient

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,11 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
 import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+const PRODUCTS_QUERY = gql`
+  query GetProducts {
+    products {
+      id
+      title
+    }
+  }
+`
 
 export default function Home() {
   const { user } = useUser();
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY)
   return (
     <div>
       <h1>Hello word</h1>
       
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
       <pre>
         {JSON.stringify(user, null, 2)}
       </pre>
